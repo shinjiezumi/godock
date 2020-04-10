1e62ca013b80cdb6fac2d00db4555e56907d4e8b
shinjiezumi <shinjiezumi.work@gmail.com>
2020-04-10 09:51:23 +0900

    ログイン処理の仮実装

---
+++ src/chat/auth.go
@@ -0,0 +1,48 @@
+package chat
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+type authHandler struct {
+	next http.Handler
+}
+
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := r.Cookie("auth"); err == http.ErrNoCookie {
+		// 未認証
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	} else if err != nil {
+		// 何かしらのエラーが発生
+		panic(err.Error())
+	} else {
+		// 認証成功。ラップされたハンドラー呼び出す
+		h.next.ServeHTTP(w, r)
+	}
+}
+
+func MustAuth(handler http.Handler) http.Handler {
+	return &authHandler{next: handler}
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+	if len(segs) != 4 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	action := segs[2]
+	provider := segs[3]
+	switch action {
+	case "login":
+		log.Println("TODO: ログイン処理", provider)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "アクション%sには非対応です", action)
+	}
+}
--- src/main.go
+++ src/main.go
@@ -13,14 +13,24 @@ func main() {
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte("Page not found"))
+			return
+		}
 		fmt.Fprintf(w, "Hello!")
 	})
 
-	http.Handle("/chat", &chat.TemplateHandler{Filename: "chat.html"})
+	http.Handle("/login", &chat.TemplateHandler{Filename: "login.html"})
+	http.HandleFunc("/auth/", chat.LoginHandler)
+
+	// MustAuthヘルパーでラップすると認証必須なページとすることが出来る
+	http.Handle("/chat", chat.MustAuth(&chat.TemplateHandler{Filename: "chat.html"}))
 	r := chat.NewRoom()
 	// SetTracerで出力先を指定するとログが出力される。
 	chat.SetTracer(r, os.Stdout)
 	http.Handle("/chat/room", r)
+
 	// チャットルームを開始
 	go r.Run()
 	log.Println("Webサーバーを開始します。ポート", *addr)
